59198aa295119e5e11965606da9ddf2920032add
DESKTOP-J3A0FEP\user <bagazfrazetyo@gmail.com>
2023-05-25 22:21:31 +0700

    implement skill

---
+++ data.js
@@ -0,0 +1,319 @@
+var dataPortfolio = {
+  skillType: ["Backend", "Frontend", "CSS", "Database"],
+
+  skill: [
+    {
+      name: "PHP",
+      level: "advanced",
+      experience: "4 Tahun",
+      type: "Backend",
+      icon: "fab fa-php",
+      description:
+        "Sebagai seorang pengembang PHP tingkat lanjutan dengan pengalaman selama 4 tahun, saya memiliki keahlian dalam menciptakan aplikasi sederhana yang efektif dan efisien",
+      project: [],
+    },
+    {
+      name: "Laravel",
+      level: "beginner",
+      experience: "4 Tahun",
+      description:
+        "Berpengalaman dalam pengembangan dan pembuatan aplikasi menggunakan framework laravel pada aplikasi monolitik maupun microservice ",
+      project: ["SIKN ANRI", "Regmus"],
+    },
+    {
+      name: "Yii",
+      level: "advanced",
+      experience: "2 Tahun",
+      description:
+        "Pengembang Yii tingkat lanjutan dengan pengalaman 2 tahun, fokus pada pengembangan website pemerintah menggunakan framework Yii1 dan Yii2.",
+      project: ["AHU Online"],
+    },
+    {
+      name: "Bootstrap",
+      level: "advanced",
+      experience: "3 Tahun",
+      description:
+        "Berpengalaman dalam mengembangkan website dengan menggunakan framework bootstrap pada aplikasi monolitik maupun microservice, serta pengalaman dalam mengembangkan website pemerintah",
+      project: ["Regmus", "AHU Online"],
+    },
+    {
+      name: "Tailwind",
+      level: "advanced",
+      experience: "2 Tahun",
+      description:
+        "Berpengalaman dalam pengembangan dalam melakukan slicing dari desain figma ke dalam kode HTML",
+      project: ["Portal DPD RI", "Landingpage S-LINK"],
+    },
+    {
+      name: "Mysqsl",
+      level: "advanced",
+      experience: "3 Tahun",
+      description:
+        "Melakukan Normalisasi database, membuat query yang efektif dan efisien, melakukan backup database dan monitoring database",
+      project: ["Regmus", "AHU Online"],
+    },
+    {
+      name: "Jquery",
+      level: "advanced",
+      experience: "3 Tahun",
+      description:
+        "Berpengalaman dalam pengembangan website menggunakan jquery untuk manipulasi DOM, AJAX, dan lain-lain",
+      project: ["AHU Online"],
+    },
+    {
+      name: "Javascript",
+      level: "advanced",
+      experience: "4 Tahun",
+      description:
+        "Berpengalaman dalam pengembangan website menggunakan javascript untuk manipulasi DOM, AJAX, dan lain-lain",
+      project: [],
+    },
+    {
+      name: "HTML 5",
+      level: "advanced",
+      experience: "5 Tahun",
+      description: "Melakukan slicing dari desain figma ke dalam kode HTML",
+      project: [],
+    },
+    {
+      name: "CSS3",
+      level: "advanced",
+      experience: "5 Tahun",
+      description: "Melakukan slicing dan customisasi desain",
+      project: [],
+    },
+    {
+      name: "Vue Js",
+      level: "intermediate",
+      experience: "1 Tahun",
+      description:
+        "Melakukan pembuatan aplikasi Toko Online menggunakan Vue Js",
+      project: ["Toko Online"],
+    },
+    {
+      name: "React Js",
+      level: "intermediate",
+      experience: "1 Tahun",
+      description:
+        "Melakukan pengembangan pada website admin Finpay menggunakan React Js",
+      project: ["Finpay"],
+    },
+    {
+      name: "Next Js",
+      level: "intermediate",
+      experience: "6 Bulan",
+      description: "Melakukan recoding website PORTAL DPD RI",
+      project: ["Cloning Portal DPD RI"],
+    },
+    {
+      name: "Livewire",
+      level: "intermediate",
+      experience: "6 Bulan",
+      description:
+        "Melakukan pembuatan aplikasi sederhana menggunakan livewire",
+      project: ["Kasir"],
+    },
+    {
+      name: "Dart",
+      level: "intermediate",
+      experience: "1 Tahun",
+      description: "Melakukan pengembangan aplikasi mobile menggunakan dart",
+      project: ["OKE JEK", "S-LINK"],
+    },
+    {
+      name: "Flutter",
+      level: "intermediate",
+      experience: "1 Tahun",
+      description: "Melakukan pengembangan aplikasi mobile menggunakan flutter",
+      project: ["OKE JEK", "S-LINK"],
+    },
+    {
+      name: "Postgresql",
+      level: "intermediate",
+      experience: "1.5 Tahun",
+      description: "Berpengalaman dalam mengeloal database postgresql",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Node Js",
+      level: "beginner",
+      experience: "6 Bulan",
+      description:
+        "Berpengalaman dalam pembuatan BOT Whatsapp, dan dalam tahap belajar untuk pengembangan website menggunakan node js",
+      project: ["Crawling Data", "Bot Whatsapp"],
+    },
+    {
+      name: "Python",
+      level: "beginner",
+      experience: "6 Bulan",
+      description: "Melakukan crawling data menggunakan python",
+      project: ["Crawling Data"],
+    },
+    {
+      name: "Angular",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "Sedang dalam tahap pembelajaran",
+      project: [],
+    },
+    {
+      name: "Golang",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "Sedang dalam tahap pembelajaran",
+      project: [],
+    },
+    {
+      name: "Java",
+      level: "beginner",
+      experience: "6 Bulan",
+      description: "Sedang dalam tahap pembelajaran",
+      project: ["Testing Socket Finpay"],
+    },
+    {
+      name: "Mongo DB",
+      level: "beginner",
+      experience: "3 Bulan",
+      description:
+        "Pengalaman dalam pembuatan database untuk aplikasi MERN (MongoDB, Express, React, Node) course",
+      project: [],
+    },
+    {
+      name: "Firebase",
+      level: "beginner",
+      experience: "1 Tahun",
+      description:
+        "Pengalaman dalam pembuatan dan pengembangan aplikasi mobile menggunakan firebase",
+      project: ["Landingpage S-LINK", "OKE JEK"],
+    },
+    {
+      name: "CI",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "Sedang dalam tahap pembelajaran",
+      project: [],
+    },
+    {
+      name: "Nuxt Js",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "-",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Typescript",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "Express Js",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "Dot Net",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "-",
+      project: ["Portal DPD RI"],
+    },
+    {
+      name: "Redis",
+      level: "beginner",
+      experience: "3 Bulan",
+      description: "Melakukan caching data pada aplikasi",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Alpine Js",
+      level: "beginner",
+      experience: "6 Bulan",
+      description: "-",
+      project: ["Portal DPD RI", "Portfolio"],
+    },
+    {
+      name: "Git",
+      level: "other",
+      experience: "3 Tahun",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "Github",
+      level: "other",
+      experience: "3 Tahun",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "Gitlab",
+      level: "other",
+      experience: "3 Tahun",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "VS Code",
+      level: "other",
+      experience: "-",
+      description: "-",
+      project: [],
+    },
+    {
+      name: "Jira",
+      level: "other",
+      experience: "-",
+      description:
+        "Melakukan monitoring dan management project menggunakan Jira",
+      project: [],
+    },
+    {
+      name: "Minio",
+      level: "other",
+      experience: "3 Bulan",
+      description: "Melakukan uploading file ke minio",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Keycloak",
+      level: "other",
+      experience: "3 Bulan",
+      description:
+        "Melakukan autentikasi dan management user menggunakan keycloak",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Solr",
+      level: "other",
+      experience: "3 Bulan",
+      description: "Melakukan searching data menggunakan solr",
+      project: ["SIKN ANRI"],
+    },
+    {
+      name: "Lumen",
+      level: "beginner",
+      experience: "6 Bulan",
+      description: "Membuat API finpay menggunakan lumen",
+      project: ["Finpay"],
+    },
+  ],
+};
+
+
+var skill = {
+  advance : [1,2,3,4,5],
+  intermediate : [1,2,3,4],
+  beginner : [1,2,3],
+  other : [1,2,3],
+}
+
+var skill = {
+  advance : [1,2,3,4,5],
+  beginner : [1,2,3],
+  intermediate : [1,2,3,4],
+  other : [1,2,3],
+}
